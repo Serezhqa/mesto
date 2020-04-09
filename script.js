bf7d14016ddc82c741d58eb0aa1d27f6be8a7466
Sergey Podryabinnikov <valentine94@inbox.ru>
2020-04-09 11:34:41 +0300

    feat/popup открывается и закрывается

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-// let editButton = document.querySelector('.profile__edit-button');
-// let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
 
-// function testFunc() {
-//   popup.classList.add('popup_opened');
-// }
+function openPopup() {
+  let popup = document.querySelector('.popup');
+  let profileName = document.querySelector('.profile__name');
+  let profileDescription = document.querySelector('.profile__description');
+  let popupInputItem = popup.querySelectorAll('.popup__input-item');
+  popupInputItem[0].value = profileName.textContent;
+  popupInputItem[1].value = profileDescription.textContent;
+  popup.classList.add('popup_opened');
+  document.body.style.overflow = 'hidden';
 
-// editButton.addEventListener('click', testFunc);
+  let closeButton = document.querySelector('.popup__close-button');
+
+  function closePopup() {
+    popup.classList.remove('popup_opened');
+    document.body.style.overflow = 'visible';
+  }
+
+  closeButton.addEventListener('click', closePopup);
+}
+
+editButton.addEventListener('click', openPopup);
