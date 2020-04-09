638828381f6d32e574bd58b342eb3020e3acb213
Sergey Podryabinnikov <valentine94@inbox.ru>
2020-04-09 13:22:51 +0300

    feat/кнопка сохранения работает

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let editButton = document.querySelector('.profile__edit-button');
+const editButton = document.querySelector('.profile__edit-button');
 
 function openPopup() {
-  let popup = document.querySelector('.popup');
-  let profileName = document.querySelector('.profile__name');
-  let profileDescription = document.querySelector('.profile__description');
-  let popupInputItem = popup.querySelectorAll('.popup__input-item');
+  //Открытие окна и запись текущих значений
+  const popup = document.querySelector('.popup');
+  const profileName = document.querySelector('.profile__name');
+  const profileDescription = document.querySelector('.profile__description');
+  const popupInputItem = popup.querySelectorAll('.popup__input-item');
   popupInputItem[0].value = profileName.textContent;
   popupInputItem[1].value = profileDescription.textContent;
   popup.classList.add('popup_opened');
   document.body.style.overflow = 'hidden';
 
-  let closeButton = document.querySelector('.popup__close-button');
+  //Закрытие окна без сохранения изменений
+  const closeButton = document.querySelector('.popup__close-button');
 
   function closePopup() {
     popup.classList.remove('popup_opened');
@@ -18,6 +20,22 @@ function openPopup() {
   }
 
   closeButton.addEventListener('click', closePopup);
+
+  //Сохранение введённых данных и обновление их на странице
+  const formElement = popup.querySelector('.popup__container');
+
+  function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    const nameInput = formElement.querySelectorAll('.popup__input-item')[0];
+    const jobInput = formElement.querySelectorAll('.popup__input-item')[1];
+
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+    closePopup();
+  }
+
+  formElement.addEventListener('submit', formSubmitHandler);
 }
 
 editButton.addEventListener('click', openPopup);
