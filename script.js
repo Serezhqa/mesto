65aa4286b578a95bd57c660d8e04c3bd1c07d804
Sergey Podryabinnikov <valentine94@inbox.ru>
2020-04-09 09:44:30 +0300

    feat/попап готов

---
+++ script.js
@@ -0,0 +1,8 @@
+// let editButton = document.querySelector('.profile__edit-button');
+// let popup = document.querySelector('.popup');
+
+// function testFunc() {
+//   popup.classList.add('popup_opened');
+// }
+
+// editButton.addEventListener('click', testFunc);
